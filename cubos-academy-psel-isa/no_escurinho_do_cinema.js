945557a93d998d1f8127a408819a0bc36a5bb691
Lucas <lucas_rocha_25@hotmail.com>
2021-06-08 21:33:07 -0300

    resolução do exercicio contagem regressiva

---
+++ cubos-academy-psel-isa/no_escurinho_do_cinema.js
@@ -0,0 +1,29 @@
+function solucao(obj) {
+  if (
+    obj.temIngresso === true &&
+    (obj.idade >= obj.censura || obj.estaComPais === true)
+  ) {
+    if (obj.temCarteirinha === true || obj.idade < 18) {
+      console.log("MEIA");
+    } else {
+      console.log("INTEIRA");
+    }
+  } else {
+    console.log("ACESSO NEGADO");
+  }
+}
+
+function processData(input) {
+  solucao(JSON.parse(input));
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+  _input += input;
+});
+
+process.stdin.on("end", function () {
+  processData(_input);
+});
